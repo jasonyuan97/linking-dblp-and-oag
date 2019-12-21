1eba0d3dab06288b55633ac49ff6f7978c452411
jasonyuan <jaosnyuan97@outlook.com>
2019-12-21 15:23:45 -0600

    oag venue parser

---
+++ oag/parse.py
@@ -0,0 +1,15 @@
+import ast
+
+pairs = set()
+with open("venue_linking_pairs.txt", "r") as paired_data:
+    for line in paired_data:
+        dictionary = ast.literal_eval(line)
+        pairs.add(dictionary["mid"])
+
+with open("mag_venues.txt", "r") as source:
+    with open("oag_venues.txt", "w") as target:
+        for line in source:
+            dictionary = ast.literal_eval(line)
+            if(dictionary["id"] in pairs):
+                normalized = {"id": dictionary["id"], "name": dictionary["NormalizedName"]}
+                target.write(str(normalized)+"\n")
